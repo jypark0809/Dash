28db2b272c8035e409c50188efef7545512eecc2
jypark0809 <jypark_0809@naver.com>
2022-08-09 18:11:33 +0900

    prototype v2.0.8

---
+++ Assets/Scripts/Items/JumpingShoes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingShoes : BaseItem
+{
+    public override void Init()
+    {
+        _anim = GetComponent<Animator>();
+        _anim.Play("Item_Fly");
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Obstacles/Bee.cs
+++ Assets/Scripts/Obstacles/Bee.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Bee : BaseObstacle
 {
+    float screenRightWorldPos;
     public int _speed = 3;
 
     public override void Init()
     {
+        Debug.Log("screenRightWorldPos : " + Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
         _anim = GetComponent<Animator>();
-        _anim.Play("Bee");
+        screenRightWorldPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x;
     }
 
     void Update()
     {
+        if (transform.position.x < screenRightWorldPos)
+        {
             transform.Translate(Vector2.left * _speed * Time.deltaTime);
+            _anim.Play("Bee");
+        }
     }
 }
